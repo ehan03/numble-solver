87084a919aa6adda3d03edecf218bd01c6a7569b
ehan03 <eugeneh442@gmail.com>
2023-04-04 04:58:22 -0400

    Improved readability + performance

--- solver.py
+++ solver.py
@@ -12,35 +12,69 @@ def numble_solve(nums, target):
             solutions, return any one of them. If there is no solution,
             return "No solution".
     """
+    # Here we use a bottom-up dynamic programming approach to essentially generate all possible
+    # valid expressions for each subset of the 6 numbers
 
+    # Make sure we have 6 numbers
     if len(nums) != 6:
         return "Please enter 6 numbers"
     
-    expr = [{} for _ in range(1 << 6)]
+    # Main idea is to encode the set of 6 numbers and any subset of them in a single integer
+    # Easy way to do this is to use a bit mask
+    # Maximum number of subsets is obviously 2^6 = 64
+    MAX_SUBSETS = 64
+
+    # Keep a list of all possible expressions for each subset of the numbers
+    # We index by the unique bit mask for each subset
+    expression = [{} for x in range(MAX_SUBSETS)]
+    
+    # Each key-value pair will be the value of the expression and the expression itself
+    # Initialize the list with the 6 numbers on their own
     for i in range(6):
-        expr[1 << i] = {nums[i]: str(nums[i])}
-    tout = (1 << 6) - 1
-    for S in range(3, tout + 1):
-        if expr[S] != {}:
+        expression[1 << i] = {nums[i]: str(nums[i])}
+
+    # Let set_U be the set of numbers we are considering
+    # We loop starting at 3 since it is first non-power of 2
+    for set_U in range(3, MAX_SUBSETS):
+        # Make sure we haven't already solved this subset
+        if expression[set_U] != {}:
             continue
-        for L in range(1, S):
-            if L & S == L:
-                R = S ^ L
-                for vL in expr[L]:
-                    for vR in expr[R]:
-                        eL = expr[L][vL]
-                        eR = expr[R][vR]
-                        expr[S][vL] = eL
-                        if vL > vR:
-                            expr[S][vL - vR] = "(%s - %s)" % (eL, eR)
-                        if L < R:
-                            expr[S][vL + vR] = "(%s + %s)" % (eL, eR)
-                            expr[S][vL * vR] = "(%s * %s)" % (eL, eR)
-                        if vR != 0 and vL % vR == 0:
-                            expr[S][vL // vR] = "(%s / %s)" % (eL, eR)
-    
-    
-    if target in expr[tout]:
-        return "%s = %i" % (expr[tout][target], target)
+
+        # Loop through all candidates for possible subsets of set_U
+        for set_A in range(1, set_U):
+            # Check if set_A is a subset of set_U
+            if set_A & set_U == set_A:
+                # set_B is the complement of set_A in set_U, i.e. the numbers in set_U that are not in set_A
+                # We do this because each number can only be used at most once
+                # Because we use bit masks, complement is just XOR
+                set_B = set_U ^ set_A
+
+                # Loop over all possible values for set_A
+                for val_L in expression[set_A]:
+                    # Loop over all possible values for set_B
+                    for val_R in expression[set_B]:
+                        # Get the expressions for val_L and val_R
+                        expr_L = expression[set_A][val_L]
+                        expr_R = expression[set_B][val_R]
+
+                        # Make sure to consider case where we don't do any operations
+                        expression[set_U][val_L] = expr_L
+
+                        # Check if subtraction is valid, i.e. positive result
+                        if val_L > val_R:
+                            expression[set_U][val_L - val_R] = f"({expr_L} - {expr_R})"
+
+                        # Symmetry, avoid repeating
+                        if set_A < set_B:
+                            expression[set_U][val_L + val_R] = f"({expr_L} + {expr_R})"
+                            expression[set_U][val_L * val_R] = f"({expr_L} * {expr_R})"
+
+                        # Check if division is valid, i.e. integer result
+                        if val_R != 0 and val_L % val_R == 0:
+                            expression[set_U][val_L // val_R] = f"({expr_L} / {expr_R})"
+
+        # Check if we have a solution
+        if target in expression[set_U]:
+            return f"{expression[set_U][target]} = {target}"
     
     return "No solution"
