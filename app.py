f7ef2dc1eab27acb90bba69c2d1f8b2ba53d2c38
ehan03 <eugeneh442@gmail.com>
2023-04-05 02:29:58 -0400

    Added stricter constraints

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Numble Solver")
 
 st.write("Check out the daily Numble challenge: [https://numble.wtf/](https://numble.wtf/)")
 
-target = st.number_input("Enter target", min_value=0, value=0)
+target = st.number_input("Enter target", min_value=100, value=None)
 
 def make_grid(cols,rows):
     grid = [0]*cols
@@ -15,12 +15,14 @@ def make_grid(cols,rows):
     return grid
 
 mygrid = make_grid(2, 3)
-num1 = mygrid[0][0].number_input("Enter number 1", min_value=0, value=0)
-num2 = mygrid[0][1].number_input("Enter number 2", min_value=0, value=0)
-num3 = mygrid[0][2].number_input("Enter number 3", min_value=0, value=0)
-num4 = mygrid[1][0].number_input("Enter number 4", min_value=0, value=0)
-num5 = mygrid[1][1].number_input("Enter number 5", min_value=0, value=0)
-num6 = mygrid[1][2].number_input("Enter number 6", min_value=0, value=0)
+num1 = mygrid[0][0].number_input("Enter number 1", min_value=1, value=None)
+num2 = mygrid[0][1].number_input("Enter number 2", min_value=1, value=None)
+num3 = mygrid[0][2].number_input("Enter number 3", min_value=1, value=None)
+num4 = mygrid[1][0].number_input("Enter number 4", min_value=1, value=None)
+num5 = mygrid[1][1].number_input("Enter number 5", min_value=1, value=None)
+num6 = mygrid[1][2].number_input("Enter number 6", min_value=1, value=None)
 
 if st.button("Solve"):
-    st.write(numble_solve([num1, num2, num3, num4, num5, num6], target))
+    if target and num1 and num2 and num3 and num4 and num5 and num6:
+        st.write("Empty fields are not allowed")
+    st.write(f"Solution: {numble_solve([num1, num2, num3, num4, num5, num6], target)}")
